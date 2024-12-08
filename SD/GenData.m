b6e12d2dc213c2b033faed4fbe7efe76c23eef76
JYKIMkentech <129360396+JYKIMkentech@users.noreply.github.com>
2024-12-09 02:50:40 +0900

    .
    .

--- SD/GenData.m
+++ SD/GenData.m
@@ -247,6 +247,47 @@ xlabel('\theta');
 ylabel('\gamma');
 title('Bimodal \gamma vs \theta');
 
+%% Plot Current Scenarios Only (Before Applying Resistance Distribution)
+figure('Name','Current scenarios only','NumberTitle','off');
+
+num_rows = 5;
+num_cols = 2;
+
+for s = 1:num_scenarios
+    subplot(num_rows, num_cols, s);
+    plot(t, ik_scenarios(s,:), 'b-', 'LineWidth', 1.5);
+    xlabel('Time (s)','FontSize',12);
+    ylabel('Current (A)','FontSize',12);
+    title(['Scenario ', num2str(s)], 'FontSize',14);
+end
+
+sgtitle('Current Scenarios','FontSize',16);
+
+
+% 예: AS1_1per에 대해 시나리오 6,7,8,9를 표시하는 2x2 subplot figure
+scenarios_to_plot = [6, 7, 8, 9];
+figure('Name','AS2_1per - Scenarios 6,7,8,9','NumberTitle','off');
+
+for idx = 1:length(scenarios_to_plot)
+    s = scenarios_to_plot(idx);
+    subplot(2, 2, idx);
+    
+    yyaxis left
+    plot(AS2_1per(s).t, AS2_1per(s).I, 'b-', 'LineWidth', 1.5);
+    ylabel('Current (A)', 'FontSize', 12);
+    hold on;
+    
+    yyaxis right
+    plot(AS2_1per(s).t, AS2_1per(s).V, 'r-', 'LineWidth', 1.5);
+    ylabel('Voltage (V)', 'FontSize', 12);
+    
+    xlabel('Time (s)', 'FontSize', 12);
+    title(['Scenario ', num2str(AS2_1per(s).SN)], 'FontSize', 14);  % 시나리오 번호 사용
+    legend('Current', 'Voltage', 'Location', 'best');
+    hold off;
+end
+
+sgtitle('AS2\_1per: Scenarios 6,7,8,9', 'FontSize', 16);
 
 %% Save
 
